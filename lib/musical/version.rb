baa7ed2a6e562fbb859bf88374202dfe6e0c3049
ryo katsuma <katsuma@gmail.com>
2013-03-05 00:33:55 +0900

    Use bundler to make gem

---
+++ lib/musical/version.rb
@@ -0,0 +1,4 @@
+# coding: utf-8
+module Musical
+  VERSION = "0.0.5"
+end
--- spec/musical_spec.rb
+++ spec/musical_spec.rb
@@ -1,7 +1,6 @@
-require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+# coding: utf-8
+require 'spec_helper'
+require 'musical'
 
-describe "Musical" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
-  end
+describe Musical do
 end
