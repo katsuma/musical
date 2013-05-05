76312615eb12a5d08eb0a0898cac07f34608997c
ryo katsuma <katsuma@gmail.com>
2013-05-05 11:49:13 +0900

    Add spec for argument parser

--- lib/musical.rb
+++ lib/musical.rb
@@ -1,20 +1,14 @@
-require 'rubygems'
 require 'trollop'
 require 'progressbar'
 require 'fileutils'
-require 'appscript'
 
-require 'musical/dvd'
-require 'musical/itunes'
+require 'musical/util'
 
 module Musical
+  extend Musical::Util
+
   def setup
-    # check env
-    ['dvdbackup', 'ffmpeg'].each do |app|
-      if `which #{app}`.empty?
-        raise RuntimeError, "\n\n'#{app}' is not installed.\n\ntry:\n  brew install #{app}\n\n"
-      end
-    end
+    return unless check_env
 
     version = open(File.join(File.dirname(__FILE__), "..", "VERSION")){ |f| f.gets }
 
@@ -24,17 +18,17 @@ module Musical
       opt :info, "Show your DVD data", :type => :boolean
       opt :ignore_convert_sound, "Rip data only, NOT convert them to wav file", :type => :boolean
       opt :ignore_use_itunes, "NOT add ripped files to iTunes and encode them", :type => :boolean
-      opt :dev, "Set location of DVD device"
-      opt :title, "Set DVD title", :default => 'LIVE'
-      opt :artist, "Set DVD artist", :default => 'Artist'
-      opt :output, "Set location of ripped data", :default => 'ripped'
+      opt :dev, "Set location of DVD device", :type => :string
+      opt :title, "Set DVD title", :type => :string, :default => 'LIVE'
+      opt :artist, "Set DVD artist", :type => :string, :default => 'Artist'
+      opt :output, "Set location of ripped data", :type => :string, :default => 'ripped'
     end
 
-    # fix fox dvdbackup
-    opts[:trim_title] = opts[:title].gsub(" ", "_")
-    opts[:trim_artist] = opts[:artist].gsub(" ", "_")
+    # for dvdbackup
+    opts[:title] = opts[:title].gsub(" ", "_").gsub("\"", '').gsub("\'", '')
+    opts[:artist] = opts[:artist].gsub(" ", "_").gsub("\"", '').gsub("\'", '')
 
     opts
   end
-
+  module_function :setup
 end
--- spec/musical_spec.rb
+++ spec/musical_spec.rb
@@ -3,4 +3,57 @@ require 'spec_helper'
 require 'musical'
 
 describe Musical do
+  describe '#setup' do
+    subject(:setup) { Musical.setup }
+    before { Musical.should_receive(:check_env).and_return(true) }
+
+    context 'when argument `info` is given' do
+      before { stub_const('ARGV', ['--info']) }
+      it { setup[:info].should be_true }
+    end
+
+    context 'when argument `ignore-convert-sound` is given' do
+      before { stub_const('ARGV', ['--ignore-convert-sound']) }
+      it { setup[:ignore_convert_sound].should be_true }
+    end
+
+    context 'when argument `ignore-use-itunes` is given' do
+      before { stub_const('ARGV', ['--ignore-use-itunes']) }
+      it { setup[:ignore_use_itunes].should be_true }
+    end
+
+    context 'when argument `dev` is given' do
+      before { stub_const('ARGV', ['--dev=/dev/foo']) }
+      it { setup[:dev].should == '/dev/foo' }
+    end
+
+    context 'when argument `title` is given' do
+      context 'if argument includes a space' do
+        before { stub_const('ARGV', ['--title="new title"']) }
+        it { setup[:title].should == "new_title" }
+      end
+
+      context 'if argument does not include a space' do
+        before { stub_const('ARGV', ['--title=new!']) }
+        it { setup[:title].should == "new!" }
+      end
+    end
+
+    context 'when argument `artist` is given' do
+      context 'if argument includes a space' do
+        before { stub_const('ARGV', ['--artist="new artist"']) }
+        it { setup[:artist].should == "new_artist" }
+      end
+
+      context 'if argument does not include a space' do
+        before { stub_const('ARGV', ['--artist=artist!']) }
+        it { setup[:artist].should == "artist!" }
+      end
+    end
+
+    context 'when argument `output` is given' do
+      before { stub_const('ARGV', ['--output=/tmp']) }
+      it { setup[:output].should == '/tmp' }
+    end
+  end
 end
