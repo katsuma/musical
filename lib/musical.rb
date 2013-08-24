c50be0444b919e1306449fc5a40eb1ce9c6487eb
ryo katsuma <katsuma@gmail.com>
2013-08-25 03:49:11 +0900

    Refine option parser specs

--- lib/musical.rb
+++ lib/musical.rb
@@ -13,25 +13,17 @@ module Musical
   def setup
     return unless check_env
 
-    version = open(File.join(File.dirname(__FILE__), "..", "VERSION")){ |f| f.gets }
-
     # parse options
-    opts = Trollop::options do
-      version "Musical #{version}"
-      opt :info, "Show your DVD data", :type => :boolean
-      opt :ignore_convert_sound, "Rip data only, NOT convert them to wav file", :type => :boolean
-      opt :ignore_use_itunes, "NOT add ripped files to iTunes and encode them", :type => :boolean
-      opt :dev, "Set location of DVD device", :type => :string
-      opt :title, "Set DVD title", :type => :string, :default => 'LIVE'
-      opt :artist, "Set DVD artist", :type => :string, :default => 'Artist'
-      opt :output, "Set location of ripped data", :type => :string, :default => 'ripped'
+    Trollop::options do
+      version "Musical #{Musical::VERSION}"
+      opt :info, "Show your DVD data", type: :boolean
+      opt :ignore_convert_sound, "Rip data only, NOT convert them to wav file", type: :boolean
+      opt :ignore_use_itunes, "NOT add ripped files to iTunes and encode them", type: :boolean
+      opt :dev, "Set location of DVD device", type: :string
+      opt :title, "Set DVD title", type: :string, default: 'LIVE'
+      opt :artist, "Set DVD artist", type: :string, default: 'Artist'
+      opt :output, "Set location of ripped data", type: :string, default: 'ripped'
     end
-
-    # for dvdbackup
-    opts[:title] = opts[:title].gsub(" ", "_").gsub("\"", '').gsub("\'", '')
-    opts[:artist] = opts[:artist].gsub(" ", "_").gsub("\"", '').gsub("\'", '')
-
-    opts
   end
   module_function :setup
 end
--- spec/musical_spec.rb
+++ spec/musical_spec.rb
@@ -9,51 +9,37 @@ describe Musical do
 
     context 'when argument `info` is given' do
       before { stub_const('ARGV', ['--info']) }
-      it { setup[:info].should be_true }
+      it { expect(setup[:info]).to be_true }
     end
 
     context 'when argument `ignore-convert-sound` is given' do
       before { stub_const('ARGV', ['--ignore-convert-sound']) }
-      it { setup[:ignore_convert_sound].should be_true }
+      it { expect(setup[:ignore_convert_sound]).to be_true }
     end
 
     context 'when argument `ignore-use-itunes` is given' do
       before { stub_const('ARGV', ['--ignore-use-itunes']) }
-      it { setup[:ignore_use_itunes].should be_true }
+      it { expect(setup[:ignore_use_itunes]).to be_true }
     end
 
     context 'when argument `dev` is given' do
       before { stub_const('ARGV', ['--dev=/dev/foo']) }
-      it { setup[:dev].should == '/dev/foo' }
+      it { expect(setup[:dev]).to eq('/dev/foo') }
     end
 
     context 'when argument `title` is given' do
-      context 'if argument includes a space' do
-        before { stub_const('ARGV', ['--title="new title"']) }
-        it { setup[:title].should == "new_title" }
-      end
-
-      context 'if argument does not include a space' do
-        before { stub_const('ARGV', ['--title=new!']) }
-        it { setup[:title].should == "new!" }
-      end
+      before { stub_const('ARGV', ['--title=new!']) }
+      it { expect(setup[:title]).to eq("new!") }
     end
 
     context 'when argument `artist` is given' do
-      context 'if argument includes a space' do
-        before { stub_const('ARGV', ['--artist="new artist"']) }
-        it { setup[:artist].should == "new_artist" }
-      end
-
-      context 'if argument does not include a space' do
-        before { stub_const('ARGV', ['--artist=artist!']) }
-        it { setup[:artist].should == "artist!" }
-      end
+      before { stub_const('ARGV', ['--artist=artist!']) }
+      it { expect(setup[:artist]).to eq("artist!") }
     end
 
     context 'when argument `output` is given' do
       before { stub_const('ARGV', ['--output=/tmp']) }
-      it { setup[:output].should == '/tmp' }
+      it { expect(setup[:output]).to eq('/tmp') }
     end
   end
 end
