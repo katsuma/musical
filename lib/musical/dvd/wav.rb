3f281e54153d70aee13aa1ede3db4fe103a43106
ryo katsuma <katsuma@gmail.com>
2013-09-08 11:12:33 +0900

    Add DVD::Wav#expand_path

--- lib/musical/dvd/wav.rb
+++ lib/musical/dvd/wav.rb
@@ -1,8 +1,12 @@
 # coding: utf-8
 module Musical
   class DVD::Wav < File
+    def expand_path
+      File.expand_path(self.path)
+    end
+
     def delete!
-      FileUtils.rm_f(self.path)
+      FileUtils.rm_f(self.expand_path)
     end
   end
 end
