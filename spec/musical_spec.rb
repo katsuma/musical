b67cbbd25f8dd259b97582ce8245a619b2d8267b
ryo katsuma <katsuma@gmail.com>
2013-09-08 13:00:31 +0900

    Fix ruby 1.9.3 issue

--- spec/musical_spec.rb
+++ spec/musical_spec.rb
@@ -9,37 +9,37 @@ describe Musical do
 
     context 'when argument `info` is given' do
       before { stub_const('ARGV', ['--info']) }
-      it { expect(setup[:info]).to be_true }
+      it { expect(setup.info).to be_true }
     end
 
     context 'when argument `ignore-convert-sound` is given' do
       before { stub_const('ARGV', ['--ignore-convert-sound']) }
-      it { expect(setup[:ignore_convert_sound]).to be_true }
+      it { expect(setup.ignore_convert_sound).to be_true }
     end
 
     context 'when argument `ignore-use-itunes` is given' do
       before { stub_const('ARGV', ['--ignore-use-itunes']) }
-      it { expect(setup[:ignore_use_itunes]).to be_true }
+      it { expect(setup.ignore_use_itunes).to be_true }
     end
 
     context 'when argument `path` is given' do
       before { stub_const('ARGV', ['--path=/dev/foo']) }
-      it { expect(setup[:path]).to eq('/dev/foo') }
+      it { expect(setup.path).to eq('/dev/foo') }
     end
 
     context 'when argument `title` is given' do
       before { stub_const('ARGV', ['--title=new!']) }
-      it { expect(setup[:title]).to eq("new!") }
+      it { expect(setup.title).to eq("new!") }
     end
 
     context 'when argument `artist` is given' do
       before { stub_const('ARGV', ['--artist=artist!']) }
-      it { expect(setup[:artist]).to eq("artist!") }
+      it { expect(setup.artist).to eq("artist!") }
     end
 
     context 'when argument `output` is given' do
       before { stub_const('ARGV', ['--output=/tmp']) }
-      it { expect(setup[:output]).to eq('/tmp') }
+      it { expect(setup.output).to eq('/tmp') }
     end
   end
 
